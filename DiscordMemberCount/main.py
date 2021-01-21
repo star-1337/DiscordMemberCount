e0fcdebfc781775e3ab517c1acde38530f7a5341
star <54599456+star-1337@users.noreply.github.com>
2021-01-21 10:02:50 +0000

    Update main.py

--- DiscordMemberCount/main.py
+++ DiscordMemberCount/main.py
@@ -29,4 +29,4 @@ for filename in os.listdir("./cogs"):
     if filename.endswith(".py"):
         client.load_extension(f'cogs.{filename[:-3]}')
         
-client.run("ODAxNzM3MDAwMjMxMjM5NzAx.YAlBxA.y8eUkFmZDL4Pds-_KwspIOai8RE")
+client.run("TOKEN")
